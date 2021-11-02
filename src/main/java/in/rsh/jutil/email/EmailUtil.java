519af0c29657a0fbcab98736263d658274ae7a0d
Rahil S <rahilrshk@gmail.com>
2021-11-03 00:50:50 +0530

    Refactoring

--- src/main/java/in/rsh/jutil/email/EmailUtil.java
+++ src/main/java/in/rsh/jutil/email/EmailUtil.java
@@ -4,6 +4,6 @@ import org.apache.commons.validator.routines.EmailValidator;
 
 public class EmailUtil {
   public static boolean isEmailValid() {
-    return EmailValidator.getInstance().isValid("senthilkumarR@cholams.murugappa.com");
+    return EmailValidator.getInstance().isValid("test@test.com");
   }
 }

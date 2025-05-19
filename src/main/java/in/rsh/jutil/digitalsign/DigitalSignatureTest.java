8e5cc0a83f3adaf9f069553a66a6f8a6399b7cc1
Rahil S <rahilrshk@gmail.com>
2025-05-19 17:58:07 +0000

    Fixed Base64 compiler error

--- src/main/java/in/rsh/jutil/digitalsign/DigitalSignatureTest.java
+++ src/main/java/in/rsh/jutil/digitalsign/DigitalSignatureTest.java
@@ -4,7 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.Signature;
-import sun.misc.BASE64Encoder;
+import java.util.Base64;
 
 public class DigitalSignatureTest {
 
@@ -13,13 +13,13 @@ public class DigitalSignatureTest {
     kpg.initialize(512);
     KeyPair keyPair = kpg.genKeyPair();
     byte[] data = "test".getBytes(StandardCharsets.UTF_8);
-    System.out.println("private= " + new BASE64Encoder().encode(keyPair.getPrivate().getEncoded()));
-    System.out.println("public= " + new BASE64Encoder().encode(keyPair.getPublic().getEncoded()));
+    System.out.println("private= " + Base64.getEncoder().encodeToString(keyPair.getPrivate().getEncoded()));
+    System.out.println("public= " + Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded()));
     Signature sig = Signature.getInstance("MD5WithRSA");
     sig.initSign(keyPair.getPrivate());
     sig.update(data);
     byte[] signatureBytes = sig.sign();
-    System.out.println("Singature: " + new BASE64Encoder().encode(signatureBytes));
+    System.out.println("Singature: " + Base64.getEncoder().encodeToString(signatureBytes));
     sig.initVerify(keyPair.getPublic());
     sig.update(data);
     System.out.println(sig.verify(signatureBytes));
